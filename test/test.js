0d30b4f9d2a8d848345a43294125d40194b66bcc
DavideViolante <fungtq3@gmail.com>
2020-04-29 22:48:04 +0200

    Fix linting

--- test/test.js
+++ test/test.js
@@ -4,67 +4,67 @@ const { getGender } = require('../index')
 describe('Tests for Gender detection from name', () => {
   it('should return male for some english male names', () => {
     const names = ['Dave', 'John', 'Jacob', 'George', 'Lucas']
-    for (let name of names) {
+    for (const name of names) {
       assert.strictEqual(getGender(name, 'en'), 'male')
     }
   })
   it('should return female for some english female names', () => {
     const names = ['Anna', 'Jennifer', 'Lisa', 'Susanne', 'Andrea']
-    for (let name of names) {
+    for (const name of names) {
       assert.strictEqual(getGender(name, 'en'), 'female')
     }
   })
   it('should return male for some italian male names', () => {
     const names = ['Davide', 'Federico', 'Giacomo', 'Giorgio', 'Andrea']
-    for (let name of names) {
+    for (const name of names) {
       assert.strictEqual(getGender(name, 'it'), 'male')
     }
   })
   it('should return female for some italian female names', () => {
     const names = ['Anna', 'Azzurra', 'Luisa', 'Marta', 'Caterina']
-    for (let name of names) {
+    for (const name of names) {
       assert.strictEqual(getGender(name, 'it'), 'female')
     }
   })
   it('should return male for some names with wrong language', () => {
     const names = ['Dave', 'John', 'Giacomo', 'George', 'Luca']
-    for (let name of names) {
+    for (const name of names) {
       assert.strictEqual(getGender(name, 'de'), 'male')
     }
   })
   it('should return female for some names with wrong language', () => {
     const names = ['Anna', 'Jennifer', 'Lisa', 'Marta', 'Catherine']
-    for (let name of names) {
+    for (const name of names) {
       assert.strictEqual(getGender(name, 'de'), 'female')
     }
   })
   it('should return male for some names with no language', () => {
     const names = ['Dave', 'John', 'Giacomo', 'George', 'Luca']
-    for (let name of names) {
+    for (const name of names) {
       assert.strictEqual(getGender(name), 'male')
     }
   })
   it('should return female for some names with no language', () => {
     const names = ['Anna', 'Jennifer', 'Lisa', 'Marta', 'Catherine']
-    for (let name of names) {
+    for (const name of names) {
       assert.strictEqual(getGender(name), 'female')
     }
   })
   it('should return unknown for unknown names', () => {
     const names = ['Asdfgh', 'I do not exist', 'None']
-    for (let name of names) {
+    for (const name of names) {
       assert.strictEqual(getGender(name), 'unknown')
     }
   })
   it('should return unknown for null names', () => {
     const names = ['', undefined, null]
-    for (let name of names) {
+    for (const name of names) {
       assert.strictEqual(getGender(name), 'unknown')
     }
   })
   it('should return male for known name with null lang', () => {
     const langs = ['', undefined, null]
-    for (let lang of langs) {
+    for (const lang of langs) {
       assert.strictEqual(getGender('Dave', lang), 'male')
     }
   })
