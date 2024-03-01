0060f0e72f43a5c0a923a4e1ed1d32338ed75372
Davide Violante <DavideViolante@users.noreply.github.com>
2024-03-01 10:37:42 +0100

    Apply suggestions from code review

--- test/test.js
+++ test/test.js
@@ -62,6 +62,12 @@ describe('Tests for Gender detection from name', () => {
       assert.strictEqual(getGender(name, 'fr'), 'female');
     }
   });
+    it('should return male for some names with wrong language', () => {
+    const names = ['Dave', 'John', 'Giacomo', 'George', 'Luca'];
+    for (const name of names) {
+      assert.strictEqual(getGender(name, 'de'), 'male');
+    }
+  });
   it('should return female for some names with wrong language', () => {
     const names = ['Anna', 'Jennifer', 'Lisa', 'Marta', 'Catherine'];
     for (const name of names) {
